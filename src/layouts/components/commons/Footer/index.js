7abe45d5795ccbe2401616628d14d41ac527383f
Iradukunda <nowdidier@gmail.com>
2024-05-28 21:37:22 +0200

    UpGRADED fOOTER

--- src/layouts/components/commons/Footer/index.js
+++ src/layouts/components/commons/Footer/index.js
@@ -16,205 +16,205 @@ import MuiImageCustom from '../../../../components/MuiImageCustom';
 const Footer = () => {
   const nav = useNavigate();
 
-    return (
-      <Box>
-        <Grid container>
-          <Grid xs={12} sm={12} md={6} lg={3} xl={3} item>
-            <List dense={true}>
-              <ListItem>
-                <MuiImageCustom
-                  width={150}
-                  src={IMAGES.getTextLogo('light')}
-                  sx={{ mr: 1, mb: 1, display: { xs: 'none', md: 'flex' } }}
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/ve-chung-toi')}
-                  primary="Ku MyJob"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/lien-he')}
-                  primary="Kontakita"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/hoi-dap')}
-                  primary="Ibibazo n'Ibisubizo"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/thoa-thuan-su-dung')}
-                  primary="Amasezerano yo Gukoresha"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/quy-dinh-bao-mat')}
-                  primary="Amabwiriza y'Ubuziranenge"
-                />
-              </ListItem>
-            </List>
-          </Grid>
-          <Grid xs={12} sm={12} md={6} lg={3} xl={3} item>
-            <List dense={true}>
-              <ListItem>
-                <Typography
-                  variant="button"
-                  display="block"
-                  gutterBottom
-                  style={{ fontWeight: 'bold' }}
-                >
-                  Igenewe Abakoresha
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/nha-tuyen-dung/tin-tuyen-dung')}
-                  primary="Itangazo ryo Gutanga Akazi"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/nha-tuyen-dung/danh-sach-ung-vien')}
-                  primary="Shakisha Dosiye"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/nha-tuyen-dung')}
-                  primary="Gucunga Abakoresha"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/tro-chuyen')}
-                  primary="Ubutumwa"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/thong-bao')}
-                  primary="Amakuru"
-                />
-              </ListItem>
-            </List>
-          </Grid>
-          <Grid xs={12} sm={12} md={6} lg={3} xl={3} item>
-            <List dense={true}>
-              <ListItem>
-                <Typography
-                  variant="button"
-                  display="block"
-                  gutterBottom
-                  style={{ fontWeight: 'bold' }}
-                >
-                  Igenewe Abashaka Akazi
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/viec-lam')}
-                  primary="Akazi"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/cong-ty')}
-                  primary="Ikigo"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/ung-vien')}
-                  primary="Gucunga Abashaka Akazi"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/ung-vien/tin-nhan')}
-                  primary="Ubutumwa"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => nav('/ung-vien/thong-bao')}
-                  primary="Amakuru"
-                />
-              </ListItem>
-            </List>
-          </Grid>
-          <Grid xs={12} sm={12} md={6} lg={3} xl={3} item>
-            <List dense={true}>
-              <ListItem>
-                <Typography
-                  variant="button"
-                  display="block"
-                  gutterBottom
-                  style={{ fontWeight: 'bold' }}
-                >
-                  Porogaramu za Telefone
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Stack direction="row" spacing={2}>
-                  <Link href={LINKS.CHPLAY_LINK} target="_blank">
-                    <MuiImageCustom width={160} src={IMAGES.chPlayDownload} />
-                  </Link>
-                  <Link href={LINKS.APPSTORE_LINK} target="_blank">
-                    <MuiImageCustom width={160} src={IMAGES.appStoreDownload} />
-                  </Link>
-                </Stack>
-              </ListItem>
-              <ListItem>
-                <Typography
-                  variant="button"
-                  display="block"
-                  gutterBottom
-                  style={{ fontWeight: 'bold' }}
-                >
-                  Byemejwe na
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Stack direction="row" spacing={2}>
-                  <Link href={LINKS.CERTIFICATE_LINK} target="_blank">
-                    <MuiImageCustom
-                      width={140}
-                      src={require('../../../../assets/images/certification-logo.png')}
-                    />
-                  </Link>
-                </Stack>
-              </ListItem>
-              <ListItem>
-                <Typography
-                  variant="button"
-                  display="block"
-                  gutterBottom
-                  style={{ fontWeight: 'bold' }}
-                >
-                  Guhuzwa na MyJob
-                </Typography>
-                </ListItem>
+  return (
+    <Box>
+      <Grid container>
+        <Grid xs={12} sm={12} md={6} lg={3} xl={3} item>
+          <List dense={true}>
+            <ListItem>
+              <MuiImageCustom
+                width={150}
+                src={IMAGES.getTextLogo('light')}
+                sx={{ mr: 1, mb: 1, display: { xs: 'none', md: 'flex' } }}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/ve-chung-toi')}
+                primary="Ku byerekeye MyJob"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/lien-he')}
+                primary="Vugana Nnatwe"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/hoi-dap')}
+                primary="Ibibazo"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/thoa-thuan-su-dung')}
+                primary="Amasezerano yo gukoresha"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/quy-dinh-bao-mat')}
+                primary="Amabwiriza y'ubuzima bwite"
+              />
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid xs={12} sm={12} md={6} lg={3} xl={3} item>
+          <List dense={true}>
+            <ListItem>
+              <Typography
+                variant="button"
+                display="block"
+                gutterBottom
+                style={{ fontWeight: 'bold' }}
+              >
+                Ku bakoresha
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/nha-tuyen-dung/tin-tuyen-dung')}
+                primary="Ishyiraho Ibyifuzo"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/nha-tuyen-dung/danh-sach-ung-vien')}
+                primary="Shakisha Abarangiza"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/nha-tuyen-dung')}
+                primary="Guhagarika Umukoresha"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/tro-chuyen')}
+                primary="Ubutumwa"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/thong-bao')}
+                primary="Amakuru"
+              />
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid xs={12} sm={12} md={6} lg={3} xl={3} item>
+          <List dense={true}>
+            <ListItem>
+              <Typography
+                variant="button"
+                display="block"
+                gutterBottom
+                style={{ fontWeight: 'bold' }}
+              >
+                Ku Bitoza
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/viec-lam')}
+                primary="Akazi"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/cong-ty')}
+                primary="Sosiyete"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/ung-vien')}
+                primary="Gukora Abatoza"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/ung-vien/tin-nhan')}
+                primary="Ubutumwa"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                sx={{ cursor: 'pointer' }}
+                onClick={() => nav('/ung-vien/thong-bao')}
+                primary="Amakuru"
+              />
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid xs={12} sm={12} md={6} lg={3} xl={3} item>
+          <List dense={true}>
+            <ListItem>
+              <Typography
+                variant="button"
+                display="block"
+                gutterBottom
+                style={{ fontWeight: 'bold' }}
+              >
+                Porogaramu za telefoni
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Stack direction="row" spacing={2}>
+                <Link href={LINKS.CHPLAY_LINK} target="_blank">
+                  <MuiImageCustom width={160} src={IMAGES.chPlayDownload} />
+                </Link>
+                <Link href={LINKS.APPSTORE_LINK} target="_blank">
+                  <MuiImageCustom width={160} src={IMAGES.appStoreDownload} />
+                </Link>
+              </Stack>
+            </ListItem>
+            <ListItem>
+              <Typography
+                variant="button"
+                display="block"
+                gutterBottom
+                style={{ fontWeight: 'bold' }}
+              >
+                Byemejwe na
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Stack direction="row" spacing={2}>
+                <Link href={LINKS.CERTIFICATE_LINK} target="_blank">
+                  <MuiImageCustom
+                    width={140}
+                    src={require('../../../../assets/images/certification-logo.png')}
+                  />
+                </Link>
+              </Stack>
+            </ListItem>
+            <ListItem>
+              <Typography
+                variant="button"
+                display="block"
+                gutterBottom
+                style={{ fontWeight: 'bold' }}
+              >
+                Hura na MyJob
+              </Typography>
+            </ListItem>
             <ListItem>
               <Stack direction="row" spacing={1}>
                 <Link href={LINKS.FACEBOOK_LINK} target="_blank">
@@ -249,4 +249,4 @@ const Footer = () => {
   );
 };
 
-export default Footer;    
+export default Footer;
